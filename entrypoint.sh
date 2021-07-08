1875786b28f158f34de0883ae96f8e4ce1e2f61f
Sho Mizutani <lowply@github.com>
2021-07-08 20:59:03 +0900

    Remove ref restrictions

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,6 @@ if [ -z "${FIREBASE_PROJECT}" ]; then
     exit 1
 fi
 
-if [ -z "${TARGET_BRANCH}" ]; then
-    TARGET_BRANCH="master"
-fi
-
-if [ "${GITHUB_REF}" != "refs/heads/${TARGET_BRANCH}" ]; then
-    echo "Current branch: ${GITHUB_REF}"
-    echo "Aborting deployment"
-    exit 1
-fi
-
 firebase deploy \
     -m "${GITHUB_SHA}" \
     --project ${FIREBASE_PROJECT} \
