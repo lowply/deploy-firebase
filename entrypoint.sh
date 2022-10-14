c1837db945de45bc56ec131c3be14956c2525c9f
Jack <elmarti.tech@gmail.com>
2022-10-14 17:25:08 +0100

    :bug: resolve true correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ if [ -z "${FIREBASE_PROJECT}" ]; then
     echo "FIREBASE_PROJECT is missing"
     exit 1
 fi
-if [ -z "${FIREBASE_ONLY_HOSTING}" ]; then
+if [ -z "${FIREBASE_ONLY_HOSTING}" == "true" ]; then
     firebase deploy \
     -m "${GITHUB_REF} (${GITHUB_SHA})" \
     --project ${FIREBASE_PROJECT} \
